7df6be69e24303661e1a2c508cdc94ce231a10ac
Aliaksandr Kukharenka <aliaksandr_kukharenka@epam.com>
2015-10-21 09:25:18 +0300

    sdf

--- Java/src/main/java/com/github/vitalliuss/helloci/App.java
+++ Java/src/main/java/com/github/vitalliuss/helloci/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello CI!" );
+        System.out.println( "Hello CI! ALEX" );
     }
     
     public static void doNothing() {
